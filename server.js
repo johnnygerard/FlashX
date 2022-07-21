c92d032adff8aeda3d44eb23c1bbca7a3835d09c
Johnny Gérard <johnnygerard_id@protonmail.com>
2022-07-21 21:02:54 +0200

    build: set up dotenv
    Add .env file
    Add .env entry to .gitignore
    Add dotenv as a devDependency
    Enable dotenv in server.js for non-production environment

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import { env } from 'process';
+
+if (env.NODE_ENV !== 'production')
+    await import('dotenv/config');
 
 const app = express();
-const port = 3000;
+const port = env.PORT || 3000;
 const host = 'localhost';
 
 app.get('/', (req, res, next) => {
