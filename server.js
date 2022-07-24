f238b400bd445cab4fbb40dfab01eca2f58e2a89
Johnny Gérard <johnnygerard_id@protonmail.com>
2022-07-24 17:59:35 +0200

    feat: add register endpoint

--- server.js
+++ server.js
@@ -1,19 +1,80 @@
 import express from 'express';
+import crypto from 'crypto';
 import { env } from 'process';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 if (env.NODE_ENV !== 'production')
     await import('dotenv/config');
 
 const app = express();
-const port = env.PORT || 3000;
-const host = 'localhost';
+const PORT = env.PORT || 3000;
+const HOST = 'localhost';
+
+const DB_USER = encodeURIComponent(env.DB_USER);
+const DB_PASSWORD = encodeURIComponent(env.DB_PASSWORD);
+
+// defaults: admin database, port 27017
+const AUTHORITY = `${DB_USER}:${DB_PASSWORD}@${env.DB_HOST}`;
+const URI = `mongodb+srv://${AUTHORITY}/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1
+});
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res, next) => {
     res.render('index', { title: 'Homepage' });
 });
 
-app.listen(port, host, () => {
-    console.log(`Server listening at: http://${host}:${port}`);
+// values based on NIST recommendations
+const ITERATIONS = 1000;
+const KDF = 'sha3-256'; // key derivation function
+const KEY_BYTE_LENGTH = 14;
+const SALT_BYTE_LENGTH = 16;
+
+app.post('/register', (req, res, next) => {
+    async function run() {
+        try {
+            const users = client.db('user').collection('users');
+
+            if (await users.findOne({ _id: req.body.username })) {
+                console.log(`${req.body.username} not available`)
+                res.redirect('/');
+                return;
+            }
+
+            const salt = await new Promise((resolve, reject) => {
+                crypto.randomBytes(SALT_BYTE_LENGTH, (err, salt) => {
+                    if (err) reject(err);
+                    else resolve(salt);
+                });
+            });
+
+            const derivedKey = await new Promise((resolve, reject) => {
+                crypto.pbkdf2(req.body.password, salt, ITERATIONS, KEY_BYTE_LENGTH,
+                    KDF, (err, derivedKey) => {
+                        if (err) reject(err);
+                        else resolve(derivedKey);
+                    });
+            });
+
+            await users.insertOne({
+                _id: req.body.username,
+                salt,
+                derivedKey
+            });
+            res.redirect('/');
+        } finally {
+            await client.close();
+        }
+    }
+    run().catch(next);
+});
+
+app.listen(PORT, HOST, () => {
+    console.log(`Server listening at: http://${HOST}:${PORT}`);
 });
