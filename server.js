67c4259ddae02308b8129d0c0e1bfe1a374c81c4
Johnny Gérard <johnnygerard_id@protonmail.com>
2022-07-31 12:40:50 +0200

    feat: add LocalStrategy

--- server.js
+++ server.js
@@ -2,10 +2,51 @@ import express from 'express';
 import crypto from 'crypto';
 import { env } from 'process';
 import client from './MongoClient.js';
+import passport from 'passport';
+import LocalStrategy from 'passport-local';
 
 const app = express();
 const PORT = env.PORT || 3000;
 const HOST = 'localhost';
+
+// values based on NIST recommendations
+const ITERATIONS = 1000;
+const KDF = 'sha3-256'; // key derivation function
+const KEY_BYTE_LENGTH = 14;
+const SALT_BYTE_LENGTH = 16;
+
+passport.use(new LocalStrategy(async (username, password, done) => {
+    try {
+        await client.connect();
+        const users = client.db('user').collection('users');
+        const user = await users.findOne({ _id: username });
+
+        if (!user) {
+            console.log('user non existent');
+            done(null, false);
+            return;
+        }
+
+        const derivedKey = await new Promise((resolve, reject) => {
+            crypto.pbkdf2(password, user.salt, ITERATIONS, KEY_BYTE_LENGTH,
+                KDF, (err, derivedKey) => {
+                    if (err) reject(err); else resolve(derivedKey);
+                });
+        });
+
+        if (derivedKey.equals(user.derivedKey)) {
+            console.log('good password');
+            done(null, user);
+        } else {
+            console.log('wrong password');
+            done(null, false);
+        }
+    } catch (err) {
+        done(err);
+    } finally {
+        await client.close();
+    }
+}));
 
 app.set('view engine', 'ejs');
 app.locals = { validationMessage: undefined };
@@ -22,12 +63,6 @@ app.get('/register', (req, res, next) => {
 app.get('/login', (req, res, next) => {
     res.render('login');
 });
-
-// values based on NIST recommendations
-const ITERATIONS = 1000;
-const KDF = 'sha3-256'; // key derivation function
-const KEY_BYTE_LENGTH = 14;
-const SALT_BYTE_LENGTH = 16;
 
 const validateUsername = username => typeof username === 'string' &&
     /^[!-~]{1,128}$/.test(username);
