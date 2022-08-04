ad1192a538d613fb624d307c077519f80fa5b4ab
Johnny Gérard <johnnygerard_id@protonmail.com>
2022-08-04 22:18:52 +0200

    style: rename "included" folder to "partials"

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ passport.use(new LocalStrategy(async (username, password, done) => {
 }));
 
 app.set('view engine', 'ejs');
-const viewOptions = { root: 'views/included' };
+const viewOptions = { root: 'views/partials' };
 app.engine('ejs', (path, data, cb) => {
     ejs.renderFile(path, data, viewOptions, cb);
 });
