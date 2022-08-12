8f976b3bb16eca61bb37d1084bacc431883f3cb3
Johnny Gérard <johnnygerard_id@protonmail.com>
2022-08-12 19:32:29 +0200

    feat: add json parser

--- server.js
+++ server.js
@@ -99,6 +99,7 @@ app.engine('ejs', (path, data, cb) => {
     ejs.renderFile(path, data, viewOptions, cb);
 });
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(session({
     cookie: {
