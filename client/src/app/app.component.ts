fc64ef8186be08e2b730c670127bcc8ac8891c10
Johnny Gérard <mail@jgerard.dev>
2022-09-25 23:43:41 +0200

    feat: root component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FlashX';
+    title = 'FlashX';
+
+    constructor(private http: HttpClientModule, protected auth: AuthService) { }
+
 }
