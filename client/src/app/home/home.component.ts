dc8a4ea612547826d529b5f2e9bd642324b9d985
Johnny Gérard <mail@jgerard.dev>
2022-09-26 17:52:47 +0200

    feat: HomeComponent

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import {
+    HttpClient, HttpErrorResponse, HttpParams
+} from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+    message = '';
+    username = '';
+    password = '';
+    locked = false;
 
-  constructor() { }
+    constructor(
+        private readonly http: HttpClient,
+        private readonly router: Router,
+        protected readonly auth: AuthService) { }
 
-  ngOnInit(): void {
-  }
+    logIn(): void {
+        if (this.locked) return;
+        this.locked = true;
 
+        const params = new HttpParams().appendAll({
+            username: this.username,
+            password: this.password
+        });
+
+        const complete = () => {
+            this.message = '';
+            this.auth.authenticated = true;
+            this.router.navigateByUrl('/training');
+        };
+
+        const error = (err: HttpErrorResponse) => {
+            if (err.status === 403) {
+                this.message = err.error as string;
+            } else {
+                console.error(err);
+                alert('Unexpected error');
+            }
+            this.locked = false;
+        };
+
+        this.http.post('/api/logIn', params, {
+            responseType: 'text'
+        }).subscribe({ complete, error });
+    }
 }
