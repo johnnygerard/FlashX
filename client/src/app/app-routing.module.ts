d4d0cfd9ced205c5170b35b78fa96f8f13267fd5
Johnny Gérard <mail@jgerard.dev>
2022-09-26 14:01:58 +0200

    feat: HomeComponent route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
+    { path: '', pathMatch: 'full', component: HomeComponent, title: 'FlashX' },
     { path: '**', component: NotFoundComponent, title: '404 Not Found' }
 ];
 
