c5e72ea41f9cebcf5e78032fee625e48d50d4e66
Johnny Gérard <mail@jgerard.dev>
2022-10-09 09:19:01 +0200

    feat: disable autofocus on mobile

--- client/src/app/autofocus.directive.ts
+++ client/src/app/autofocus.directive.ts
@@ -4,11 +4,16 @@ import { AfterViewInit, Directive, ElementRef } from '@angular/core';
     selector: '[appAutofocus]'
 })
 export class AutofocusDirective implements AfterViewInit {
+    private readonly BREAKPOINT_EMS = 40;
 
     constructor(private readonly host: ElementRef) { }
 
     ngAfterViewInit(): void {
-        this.host.nativeElement.focus();
+        const fontSize = getComputedStyle(document.body).fontSize;
+
+        // do not autofocus on mobile
+        if ((innerWidth / Number.parseInt(fontSize)) > this.BREAKPOINT_EMS)
+            this.host.nativeElement.focus();
     }
 
 }
