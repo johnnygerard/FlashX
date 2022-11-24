76aca48629f208e8a0f8c975c698da65f80bb4ed
Johnny Gérard <mail@jgerard.dev>
2022-11-24 21:54:37 +0100

    feat: update user doc initialization

--- server/publicAPI.ts
+++ server/publicAPI.ts
@@ -37,7 +37,7 @@ router.post('/register', async (req, res, next) => {
             _id,
             salt,
             derivedKey,
-            fsets: [],
+            fsets: {},
             registrationDate: new Date
         };
 
