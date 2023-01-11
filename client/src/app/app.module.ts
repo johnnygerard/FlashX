6437f17e194b64ffadf33c14b5f13b3469d3b6b7
Johnny Gérard <mail@jgerard.dev>
2023-01-11 02:39:51 +0100

    feat: login and signup buttons

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // Angular Material Components
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
     declarations: [
@@ -19,6 +20,7 @@ import { MatIconModule } from '@angular/material/icon';
         BrowserAnimationsModule,
         MatToolbarModule,
         MatIconModule,
+        MatButtonModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
